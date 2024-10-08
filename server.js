4c81054db32f1f930844ded82951bceb363f0d53
Husayn Irfan <112241008+husaynirfan1@users.noreply.github.com>
2024-10-08 11:59:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-// server.js
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json());   
+app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));   
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-res.json({ message: "Welcome to Nodejs Simple REST API application." });
+  res.json({ message: "Welcome to Nodejs Simple REST API application." });
 });
 
-require("./app/routes/property.routes.js")(app);   
+require("./app/routes/property.routes.js")(app);
 
-// Listen on the port provided by Render
-app.listen(process.env.PORT || 8080, () => {
-console.log(Server is running on port ${process.env.PORT || 8080}.);
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
